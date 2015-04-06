ec64b214c637e620fc15988c589e9939bc053f93
mirasallib <miranagui@gmail.com>
2015-04-06 11:44:20 +0000

    Add uploading a picture for the page

--- blog/app/models/page.rb
+++ blog/app/models/page.rb
@@ -1,11 +1,21 @@
 class Page < ActiveRecord::Base
-   
+        mount_uploader :picture, PictureUploader
+        
    validates :providername, presence: true,
                     length: { minimum: 5 }
                 
  validates :providerinfo, presence: true,
                     length: { minimum: 10 }
-                 mount_uploader :picture, PictureUploader
-   
+            
+   validate  :picture_size
+
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
     end
 
